639e28fa711fe3b1a89849af643cbeeb73a6c22a
马嘉聪 <9a6c5609806a@gmail.com>
2023-07-16 16:08:57 +0800

    init repo

---
+++ RTE/_air001_modbus/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'air001_mdk_template' 
+ * Target:  'air001_modbus' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "air001xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
